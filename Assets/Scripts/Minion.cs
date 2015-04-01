073e223f4977b883bfce93b6e92a554568182276
Shane Coates <shane.coates@hotmail.com>
2015-04-01 11:15:36 +1100

    Functionality
    Added functionality for the minion to find the closest GameObject and
    navigate to it on the NavMesh

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -2,10 +2,25 @@
 using System.Collections;
 
 public class Minion : MonoBehaviour {
-	public GameObject dest;
 	// Use this for initialization
 	void Start () {
-		GetComponent<NavMeshAgent>().destination = dest.transform.position;
+
+		GameObject[] gos;
+		gos = GameObject.FindGameObjectsWithTag("Tower"); 
+		GameObject closest = new GameObject(); 
+		float distance = Mathf.Infinity; 
+		Vector3 position = transform.position; 
+		
+		foreach (GameObject go in gos)  { 
+			Vector3 diff = (go.transform.position - position);
+			float currDistance = diff.sqrMagnitude; 
+			if (currDistance < distance) { 
+				closest = go; 
+				distance = currDistance; 
+			} 
+		} 
+		Debug.Log (closest.transform.position);
+		GetComponent<NavMeshAgent> ().destination = closest.transform.position;
 	}
 	
 	// Update is called once per frame
