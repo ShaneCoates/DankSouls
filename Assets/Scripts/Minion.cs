be2395a705aefe5425d48ab8dc2b8bc2770fa163
Shane Coates <shane.coates@hotmail.com>
2015-04-01 11:37:37 +1100

    Rotate towers
    Towers rotate to face minions

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class Minion : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
+		transform.position = new Vector3 (Random.Range (-20, 20), 0.5f, Random.Range (-9, 9));
+		FindClosest ();
+	}
 
+	void FindClosest() {
 		GameObject[] gos;
 		gos = GameObject.FindGameObjectsWithTag("Tower"); 
 		GameObject closest = new GameObject(); 
@@ -19,10 +23,9 @@ public class Minion : MonoBehaviour {
 				distance = currDistance; 
 			} 
 		} 
-		Debug.Log (closest.transform.position);
 		GetComponent<NavMeshAgent> ().destination = closest.transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+	GameObject closest;
+	Transform target;
+	float speed = 1f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		target = FindClosest ();
+		Vector3 targetDir = target.position - transform.position;
+
+		float step = speed * Time.deltaTime;
+
+		Vector3 newDir = Vector3.RotateTowards (transform.forward, targetDir, step, 0.0f);
+		Debug.DrawRay (transform.position, newDir, Color.red);
+
+		transform.rotation = Quaternion.LookRotation (newDir);
+
+	}
+	Transform FindClosest() {
+		GameObject[] gos;
+		gos = GameObject.FindGameObjectsWithTag("Minion"); 
+		float distance = Mathf.Infinity; 
+		Vector3 position = transform.position; 
+		
+		foreach (GameObject go in gos)  { 
+			Vector3 diff = (go.transform.position - position);
+			float currDistance = diff.sqrMagnitude; 
+			if (currDistance < distance) { 
+				closest = go; 
+				distance = currDistance; 
+			} 
+		} 
+		Transform trans = closest.transform;
+		return trans;
+	}
+}
