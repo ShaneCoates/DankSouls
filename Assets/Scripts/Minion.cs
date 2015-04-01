2292e8691def2dba079ea6846b33d861a1869f05
Shane Coates <shane.coates@hotmail.com>
2015-04-01 12:11:46 +1100

    Minions targetting
    Minions destroy closest tower then move on to next closest

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -2,8 +2,10 @@
 using System.Collections;
 
 public class Minion : MonoBehaviour {
+	GameObject target;
 	// Use this for initialization
 	void Start () {
+		Debug.Log (GetComponent<BoxCollider> ().isTrigger);
 		transform.position = new Vector3 (Random.Range (-20, 20), 0.5f, Random.Range (-9, 9));
 		FindClosest ();
 	}
@@ -11,23 +13,38 @@ public class Minion : MonoBehaviour {
 	void FindClosest() {
 		GameObject[] gos;
 		gos = GameObject.FindGameObjectsWithTag("Tower"); 
-		GameObject closest = new GameObject(); 
 		float distance = Mathf.Infinity; 
 		Vector3 position = transform.position; 
 		
 		foreach (GameObject go in gos)  { 
-			Vector3 diff = (go.transform.position - position);
-			float currDistance = diff.sqrMagnitude; 
-			if (currDistance < distance) { 
-				closest = go; 
-				distance = currDistance; 
-			} 
+			if(go.activeInHierarchy == true) {
+				Vector3 diff = (go.transform.position - position);
+				float currDistance = diff.sqrMagnitude; 
+				if (currDistance < distance) { 
+					target = go; 
+					distance = currDistance; 
+				} 
+			} else {
+				Debug.Log ("dead");
+			}
 		} 
-		GetComponent<NavMeshAgent> ().destination = closest.transform.position;
+		Debug.Log(GetComponent<NavMeshAgent>().destination);
+		GetComponent<NavMeshAgent> ().destination = target.transform.position;
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log("hit");
+
+		if (other.gameObject.tag == "Tower") {
+			other.gameObject.SetActive(false);
+			GameObject.Destroy(other.gameObject);
+			Debug.Log(GetComponent<NavMeshAgent>().destination);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+		FindClosest();
 	}
 }
