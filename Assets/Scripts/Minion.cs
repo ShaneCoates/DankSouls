25bc7cf3510ff5511019b2065b86b81f310abe33
Shane Coates <shane.coates@hotmail.com>
2015-04-01 11:00:47 +1100

    NavMesh
    Addded NavMesh capabilities and small AI minion script

---
+++ Assets/Scripts/Minion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minion : MonoBehaviour {
+	public GameObject dest;
+	// Use this for initialization
+	void Start () {
+		GetComponent<NavMeshAgent>().destination = dest.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
